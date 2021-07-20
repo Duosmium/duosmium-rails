b723d5369c07ba1878db9be7a19e1f58f0c20a50
Shreyas Mayya <smayya337@gmail.com>
2021-07-20 16:34:49 -0400

    fix scrolling weirdness, remove delayed_job

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module DuosmiumRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_job.queue_adapter = :delayed_job
+    # config.active_job.queue_adapter = :delayed_job
   end
 end
